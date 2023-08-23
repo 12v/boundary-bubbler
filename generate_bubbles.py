f158f6ec93f9102b4bacfecfaa24e590b55fc891
12v <11772383+12v@users.noreply.github.com>
2023-08-23 06:09:45 +0100

    Tuning and output

--- generate_bubbles.py
+++ generate_bubbles.py
@@ -3,15 +3,15 @@ import requests
 import zipfile
 import io
 from shapely.geometry import shape
-from shapely import Point, GeometryCollection, LineString, Polygon, MultiPolygon, union_all, minimum_rotated_rectangle, buffer, is_empty, intersection
+from shapely import Point, GeometryCollection, LineString, MultiPolygon, union_all, minimum_rotated_rectangle, buffer, is_empty
 from shapely.validation import make_valid
 import os
 import matplotlib.pyplot as plt
-import math
 import numpy as np
+import pyproj
+import csv
 
 BUBBLE_LIMIT = 200
-MIN_RADIUS = 1 * 1000
 
 england_shapefile_url = 'https://boundarycommissionforengland.independent.gov.uk/wp-content/uploads/2023/06/984162_2023_06_27_Final_recommendations_England_shp.zip'
 scotland_shapefile_path = 'https://www.bcomm-scotland.independent.gov.uk/sites/default/files/2023_review_final/bcs_final_recs_2023_review.zip'
@@ -31,8 +31,6 @@ download_and_extract(england_shapefile_url, 'england')
 download_and_extract(scotland_shapefile_path, 'scotland')
 download_and_extract(wales_shapefile_path, 'wales')
 
-output_path = 'output/postcode_to_constituency_mapping.csv'
-
 def create_constituency_list(shapefile_path, constituency_name_key):
    constituencies = []
    with fiona.open('data/' + shapefile_path) as boundaries:
@@ -47,23 +45,13 @@ wales_constituencies = create_constituency_list('wales/' + wales_shapefile_filen
 
 constituencies = england_constituencies + scotland_constituencies + wales_constituencies
 
-def test_bubbles(bounds, spacing, radius):
-  minx, miny, maxx, maxy = bounds
-  grid_points = []
-  for i, y in enumerate(np.arange(miny, maxy, math.sqrt(3) * spacing * radius/10)):
-    offset = 0 if i % 2 == 0 else spacing * radius / (2 * 10)
-    for x in np.arange(minx + offset, maxx, math.sqrt(3) * spacing * radius/10):
-      grid_points.append(Point(x, y))
-  circles = [point.buffer(radius) for point in grid_points]
-  circle_iterator = (circle for circle in circles if shape.contains(circle))
-  valid_circles = [next(circle_iterator, None) for j in range(201)]
-  valid_circles = [circle for circle in valid_circles if circle is not None]
-  if len(valid_circles) > 200:
-    return None
-  else:
-    return valid_circles
+if not os.path.exists('output'):
+    os.makedirs('output')
 
-def calculate_radius_upper_bound(mrr):
+transformer = pyproj.Transformer.from_crs("epsg:27700", "epsg:4326")
+
+def calculate_radius_upper_bound(boundary):
+  mrr = minimum_rotated_rectangle(boundary)
   x, y = mrr.exterior.xy
   edge_lengths = (
     Point(x[0], y[0]).distance(Point(x[1], y[1])),
@@ -73,80 +61,91 @@ def calculate_radius_upper_bound(mrr):
   width = min(edge_lengths)
   return int((width // 2000) * 1000)
 
-def get_possible_centers(mrr, shape, radius):
-  buffered_mrr = buffer(mrr, -radius)
-  buffered_shape = buffer(shape, -radius)
-
-  return intersection(buffered_mrr, buffered_shape)
-
-for constituency in constituencies:
-  print(constituency[0])
-  shape = constituency[1]
-  mrr = minimum_rotated_rectangle(shape)
-  radius_upper_bound = calculate_radius_upper_bound(mrr)
-
-  print("Upper bound for circle radius", radius_upper_bound)
-
-  test_radius = radius_upper_bound
-  possible_centers = None
+def calculate_step(polygons, radius, iteration_count, bubble_length):
+  total_polygon_length = sum([polygon.exterior.length for polygon in polygons])
+  iteration_bubble_count = total_polygon_length / radius
+  step = radius
+  is_last_iteration = radius == 1000 or iteration_count == 2 or (bubble_length + iteration_bubble_count) > BUBBLE_LIMIT
   
-  valid_circles = []
+  if is_last_iteration:
+    step = total_polygon_length / (BUBBLE_LIMIT - bubble_length)
 
-  while test_radius > 0:
-    possible_centers = get_possible_centers(mrr, shape, test_radius)
-    if not is_empty(possible_centers):
-      polygons = possible_centers.geoms if isinstance(possible_centers, MultiPolygon) else [possible_centers]
+  return step
+
+def calculate_bubbles(boundary):
+  radius = calculate_radius_upper_bound(boundary)
+  island_of_possibility = None
+  iteration_count = 0
+  
+  bubbles = []
+  bubblesData = []
+
+  while radius > 0 and iteration_count < 3:
+    island_of_possibility = buffer(boundary, -(radius + 30))
+
+    if not is_empty(island_of_possibility):
+      print(radius)
+      polygons = island_of_possibility.geoms if isinstance(island_of_possibility, MultiPolygon) else [island_of_possibility]
+
+      step = calculate_step(polygons, radius, iteration_count, len(bubbles))
       for polygon in polygons:
-        for interpolation in np.arange(0, polygon.exterior.length, 1000):
+        for interpolation in np.arange(0, polygon.exterior.length, step):
           point = polygon.exterior.interpolate(interpolation)
-          valid_circles.append(point.buffer(test_radius))
-    test_radius -= 1000
+          bubble = point.buffer(radius)
+          if boundary.contains(bubble):
+            bubbles.append(bubble)
+            bubblesData.append([point.x, point.y, int(radius / 1000)])
 
-  bounds = shape.bounds
+    if len(bubbles) > 0:
+      iteration_count += 1
+      radius = (radius // 1500) * 1000
+    else:
+      radius -= 1000
 
-  radius = 1000
-  spacing_factor = 0
-  # valid_circles = None
+  return bubbles[:BUBBLE_LIMIT], bubblesData[:BUBBLE_LIMIT]
 
-  # while (valid_circles is None):
-  #   if spacing_factor < 10:
-  #     spacing_factor += 1
-  #   else:
-  #     radius += 1000
-  #     spacing_factor = 1
-  #   valid_circles = test_bubbles(bounds, spacing_factor, radius)
+with open('output/bubbles.csv', 'w') as csv_output:
+  writer = csv.writer(csv_output)
+  writer.writerow(['bubble', 'constituency'])
 
-  print(len(valid_circles), union_all(valid_circles).area/shape.area)
+  for constituency in constituencies:
+    constituency_name = constituency[0]
+    boundary = constituency[1]
+    print(constituency_name)
 
-  fig, ax = plt.subplots()
-  ax.set_aspect('equal', adjustable='box')
-  if isinstance(shape, GeometryCollection) or isinstance(shape, MultiPolygon):
-    for geom in shape.geoms:
-      sub_shape = geom
-      if isinstance(geom, LineString):
+    bubbles, bubblesData = calculate_bubbles(boundary)
+
+    for (x, y, radius) in bubblesData:
+      lat, long = transformer.transform(x, y)
+      writer.writerow(['({}, {}) +{}km'.format(lat, long, radius), constituency_name])
+
+    fig, ax = plt.subplots(1, 2)
+    ax[0].set_aspect('equal', adjustable='box')
+    ax[1].set_aspect('equal', adjustable='box')
+    fig.suptitle(constituency_name)
+    coverage_percentage = 100 * union_all(bubbles).area / boundary.area
+
+    coverage_percentage = 100 * union_all(bubbles).area / boundary.area
+    fig.text(0.5, 0.9, '{:.0f}% coverage'.format(coverage_percentage), ha='center', fontsize=12)
+
+    ax[0].xaxis.set_visible(False)
+    ax[0].yaxis.set_visible(False)
+
+    ax[1].xaxis.set_visible(False)
+    ax[1].yaxis.set_visible(False)
+
+    polygons = boundary.geoms if isinstance(boundary, GeometryCollection) or isinstance(boundary, MultiPolygon) else [boundary]
+    for polygon in polygons:
+      if isinstance(polygon, LineString):
         continue
-      x, y = sub_shape.exterior.xy
-      ax.plot(x, y, color='blue')
-  else:
-    x, y = shape.exterior.xy
-    ax.plot(x, y, color='blue')
-  for valid_circle in valid_circles:
-    x, y = valid_circle.exterior.xy
-    ax.plot(x, y, color='red')
-  x, y = mrr.exterior.xy
-  ax.plot(x, y, color='green')
-  polygons = []
-  if isinstance(possible_centers, MultiPolygon):
-    for geom in possible_centers.geoms:
-      x, y = geom.exterior.xy
-      ax.plot(x, y, color='yellow')
-  else:
-    x, y = possible_centers.exterior.xy
-    ax.plot(x, y, color='yellow')
-  plt.show()
+      x, y = polygon.exterior.xy
+      ax[0].plot(x, y, color='blue')
+      ax[1].plot(x, y, color='blue')
 
+    for valid_circle in bubbles:
+      x, y = valid_circle.exterior.xy
+      ax[0].plot(x, y, color='red', linewidth=0.5)
+      ax[1].fill(x, y, color='red')
 
-# half the smallest dimension of the minimum-rotated rectangle sets the upper-bound for the radius
-# we can also constrain the centre to be at least the radius away from the nearest minimum-rotate rectangle edge
-# radius = 1km sets the lower-bound
-# binary search to find the radius and position of largest enclosed circle
+    fig.savefig('output/' + constituency_name + '.jpg', dpi=300)
+    plt.close(fig)
